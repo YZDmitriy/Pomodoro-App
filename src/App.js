8b3a00788372b243ec595bb33caf3240a4415384
Dmitriy <dyuzef@mail.ru>
2022-05-30 11:04:44 +0300

    app styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Title>POMODORO</Title>
+    </>
   );
 }
 
 export default App;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  color: white;
+`;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html,body {
+background-color: #220045;
+font-size: 62.5%;
+}
+
+body {
+  font-size: 1.6rem;
+}
+
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
